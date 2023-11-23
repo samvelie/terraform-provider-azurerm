4351800b92f76f45e68aa1994b84559fd2372e42
tombuildsstuff <git@tom.ibuildstuff.eu>
2023-11-23 17:14:06 +0100

    hdinsight: refactoring `Extensions` to use `hashicorp/go-azure-sdk`

--- internal/services/hdinsight/hdinsight_hbase_cluster_resource.go
+++ internal/services/hdinsight/hdinsight_hbase_cluster_resource.go
@@ -5,14 +5,15 @@ package hdinsight
 
 import (
 	"fmt"
-	"github.com/hashicorp/go-azure-helpers/resourcemanager/location"
-	"github.com/hashicorp/go-azure-sdk/resource-manager/hdinsight/2021-06-01/configurations"
 	"log"
 	"strings"
 	"time"
 
 	"github.com/Azure/azure-sdk-for-go/services/hdinsight/mgmt/2018-06-01/hdinsight" // nolint: staticcheck
 	"github.com/hashicorp/go-azure-helpers/resourcemanager/commonschema"
+	"github.com/hashicorp/go-azure-helpers/resourcemanager/location"
+	"github.com/hashicorp/go-azure-sdk/resource-manager/hdinsight/2021-06-01/configurations"
+	"github.com/hashicorp/go-azure-sdk/resource-manager/hdinsight/2021-06-01/extensions"
 	"github.com/hashicorp/terraform-provider-azurerm/helpers/azure"
 	"github.com/hashicorp/terraform-provider-azurerm/helpers/tf"
 	"github.com/hashicorp/terraform-provider-azurerm/internal/clients"
@@ -148,7 +149,7 @@ func resourceHDInsightHBaseCluster() *pluginsdk.Resource {
 func resourceHDInsightHBaseClusterCreate(d *pluginsdk.ResourceData, meta interface{}) error {
 	client := meta.(*clients.Client).HDInsight.ClustersClient
 	subscriptionId := meta.(*clients.Client).Account.SubscriptionId
-	extensionsClient := meta.(*clients.Client).HDInsight.ExtensionsClient
+	extensionsClient := meta.(*clients.Client).HDInsight.Extensions
 	ctx, cancel := timeouts.ForCreate(meta.(*clients.Client).StopContext, d)
 	defer cancel()
 
@@ -273,16 +274,17 @@ func resourceHDInsightHBaseClusterCreate(d *pluginsdk.ResourceData, meta interfa
 	d.SetId(id.ID())
 
 	// We can only enable monitoring after creation
+	extensionsClusterId := extensions.NewClusterID(id.SubscriptionId, id.ResourceGroup, id.Name)
 	if v, ok := d.GetOk("monitor"); ok {
 		monitorRaw := v.([]interface{})
-		if err := enableHDInsightMonitoring(ctx, extensionsClient, resourceGroup, name, monitorRaw); err != nil {
+		if err := enableHDInsightMonitoring(ctx, extensionsClient, extensionsClusterId, monitorRaw); err != nil {
 			return err
 		}
 	}
 
 	if v, ok := d.GetOk("extension"); ok {
 		extensionRaw := v.([]interface{})
-		if err := enableHDInsightAzureMonitor(ctx, extensionsClient, resourceGroup, name, extensionRaw); err != nil {
+		if err := enableHDInsightAzureMonitor(ctx, extensionsClient, extensionsClusterId, extensionRaw); err != nil {
 			return err
 		}
 	}
@@ -293,7 +295,7 @@ func resourceHDInsightHBaseClusterCreate(d *pluginsdk.ResourceData, meta interfa
 func resourceHDInsightHBaseClusterRead(d *pluginsdk.ResourceData, meta interface{}) error {
 	clustersClient := meta.(*clients.Client).HDInsight.ClustersClient
 	configurationsClient := meta.(*clients.Client).HDInsight.Configurations
-	extensionsClient := meta.(*clients.Client).HDInsight.ExtensionsClient
+	extensionsClient := meta.(*clients.Client).HDInsight.Extensions
 	ctx, cancel := timeouts.ForRead(meta.(*clients.Client).StopContext, d)
 	defer cancel()
 
@@ -397,19 +399,20 @@ func resourceHDInsightHBaseClusterRead(d *pluginsdk.ResourceData, meta interface
 		sshEndpoint := FindHDInsightConnectivityEndpoint("SSH", props.ConnectivityEndpoints)
 		d.Set("ssh_endpoint", sshEndpoint)
 
-		monitor, err := extensionsClient.GetMonitoringStatus(ctx, id.ResourceGroup, id.Name)
+		extensionsClusterId := extensions.NewClusterID(id.SubscriptionId, id.ResourceGroup, id.Name)
+		monitor, err := extensionsClient.GetMonitoringStatus(ctx, extensionsClusterId)
 		if err != nil {
 			return fmt.Errorf("retrieving Monitoring Status for HBase %s: %+v", id, err)
 		}
 
-		d.Set("monitor", flattenHDInsightMonitoring(monitor))
+		d.Set("monitor", flattenHDInsightMonitoring(monitor.Model))
 
-		extension, err := extensionsClient.GetAzureMonitorStatus(ctx, id.ResourceGroup, id.Name)
+		extension, err := extensionsClient.GetAzureMonitorStatus(ctx, extensionsClusterId)
 		if err != nil {
 			return fmt.Errorf("retrieving Azure Monitor Status for HBase %s: %+v", id, err)
 		}
 
-		d.Set("extension", flattenHDInsightAzureMonitor(extension))
+		d.Set("extension", flattenHDInsightAzureMonitor(extension.Model))
 
 		if err := d.Set("security_profile", flattenHDInsightSecurityProfile(props.SecurityProfile, d)); err != nil {
 			return fmt.Errorf("setting `security_profile`: %+v", err)
